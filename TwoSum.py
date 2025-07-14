cf379a885dbd7178518cd348210c13902bf03c24
Sriram Rajendran <4156692+sriram488@users.noreply.github.com>
2025-07-14 13:48:04 -0500

    Create TwoSum.py

---
+++ TwoSum.py
@@ -0,0 +1,21 @@
+# comparing the adjacent elements in a array
+def twoSum(arr, target):
+    n = len(arr)
+    for i in range(n):
+        for j in range( i+1, n):
+            if arr[i] + arr[j] == target:
+                return True
+    return False           
+
+print(twoSum([1,2,3,4,5], 9))
+
+#HashSet
+
+def find_sum_of_two(A, val):
+    s = set() # does not allow duplicates
+    for num in A:
+        complement = val - num
+        if complement in s:
+            return True
+        s.add(num)  
+    return False
